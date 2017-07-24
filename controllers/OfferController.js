ffc14b6c39e661d0a0dfbea98b34f06dff3d98e3
iamsim <freelancer.ajith@gmail.com>
2017-07-25 01:03:19 +0530

    send offer notification

--- controllers/OfferController.js
+++ controllers/OfferController.js
@@ -112,5 +112,76 @@ module.exports = {
                 });
             });
         }
+    },
+    sendOfferNotification: function(req, res) {
+        var result = {};
+        if (req.body.Id == "") {
+            result.Code = statusCodes.errorCodes[1].Code;
+            result.Message = statusCodes.errorCodes[1].Message;
+            result.Data = null;
+            res.send(result);
+            return;
+        } else {
+            var deviceIds = [];
+            var queryString2 = 'SELECT DeviceId FROM Student';
+            database.connectionString.query(queryString2, function(err2, rows2) {
+                if (!err2) {
+                    if (rows2.length != 0) {
+                        for (var i = 0; i < rows2.length; i++) {
+                            if (rows2[i].DeviceId != null) {
+                                deviceIds.push(rows2[i].DeviceId);
+                            }
+                        }
+                        var title = req.body.Title;
+                        var description = req.body.Description;
+                        var message = {
+                            registration_ids: deviceIds,
+                            notification: {
+                                title: title,
+                                body: description,
+                                sound: "default",
+                                color: "#009e60",
+                                icon: "fcm_push_icon",
+                                click_action: "FCM_PLUGIN_ACTIVITY"
+                            },
+                            data: { //you can send only notification or only data(or include both)
+                                Id: req.body.Id,
+                                OfferCode: req.body.OfferCode,
+                                Title: req.body.Title,
+                                Description: req.body.Description,
+                                ImageURL: req.body.ImageURL,
+                                VideoURL: req.body.VideoURL,
+                                PhoneNumber: req.body.PhoneNumber,
+                                Website: req.body.Website,
+                                Address: req.body.Address,
+                                CategoryId: req.body.CategoryId
+                            },
+                            priority: "high"
+                        };
+                        fcm.send(message, function(error, response) {
+                            if (error) {
+                                result.Code = statusCodes.successCodes[0].Code;
+                                result.Message = statusCodes.successCodes[0].Message;
+                                result.Data = rows;
+                                res.send(result);
+                            } else {
+                                console.log(error);
+                                result.Code = statusCodes.successCodes[0].Code;
+                                result.Message = statusCodes.successCodes[0].Message;
+                                result.Data = rows;
+                                res.send(result);
+                            }
+                        });
+                    } else {
+                        result.Code = statusCodes.successCodes[0].Code;
+                        result.Message = statusCodes.successCodes[0].Message;
+                        result.Data = rows;
+                        res.send(result);
+                    }
+                } else {
+                    res.send(err2);
+                }
+            });
+        }
     }
 };
