fdf830cbe78ef5d7e19244342ce6c23dbe3b1e72
iamsim <freelancer.ajith@gmail.com>
2017-08-01 17:39:16 +0530

    connecting to prod db

--- database_scripts/connection_string.js
+++ database_scripts/connection_string.js
@@ -11,21 +11,21 @@ var mysql = require('mysql');
 // });
 
 //dev
-module.exports.connectionString = mysql.createConnection({
-    host: 'd6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'vx5xs7b1ypl0wr5c',
-    password: 'n7g0su30lxlzyqco',
-    database: 'jg1a6uswqriozle4',
-    multipleStatements: true
-});
+// module.exports.connectionString = mysql.createConnection({
+//     host: 'd6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     user: 'vx5xs7b1ypl0wr5c',
+//     password: 'n7g0su30lxlzyqco',
+//     database: 'jg1a6uswqriozle4',
+//     multipleStatements: true
+// });
 
 //staging
 
 //prod
-// module.exports.connectionString = mysql.createConnection({
-//     host: 'd6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     user: 'kst9anlfs1a6990y',
-//     password: 'f1a4roh5p99zdaxx',
-//     database: 'sckr1eyim57sm24l',
-//     multipleStatements: true
-// });
+module.exports.connectionString = mysql.createConnection({
+    host: 'd6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'kst9anlfs1a6990y',
+    password: 'f1a4roh5p99zdaxx',
+    database: 'sckr1eyim57sm24l',
+    multipleStatements: true
+});
